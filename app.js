cad3751d6844bd4add8a88b17212defe306d0284
pretty peakanyo <peakanyopretty@gmail.com>
2025-04-29 22:34:52 +0200

    updated my work

--- app.js
+++ app.js
@@ -3,190 +3,124 @@ const express = require('express');
 const app = express();
 app.use(express.json());  //middleware
 
-const movies =JSON.parse(fs.readFileSync(`${__dirname}/the-data/movies.json`, 'utf-8'));
 
-app.get('/api/v1/movies', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: movies.length,
-        data:{
-           movies:movies 
-        }
 
-    });
-});
+let movies = [
+  { id: 1, title: "The Dark Knight", year: 2008, genre: "Action" },
+  { id: 2, title: "Inception", year: 2010, genre: "Sci-Fi" },
+];
 
-// using parameters for a single movie
-app.get("/api/v1/movies/:id", (req, res) => {
-    console.log(req.params);
-    const id = req.params.id * 1;
+let series = [
+  {
+    id: 1,
+    title: "Stranger Things",
+    creator: "The Duffer Brothers",
+    genre: "Sci-Fi/Horror",
+  },
+  {
+    id: 2,
+    title: "The Crown",
+    creator: "Peter Morgan",
+    genre: "Historical Drama",
+  },
+];
 
-//if id is not found
-    if(id>movies.length){
-        return res.status(404).json({
-        status:'fail',
-        message:'invalid ID'
-        });
-    }
+let songs = [
+  {
+    id: 1,
+    title: "Bohemian Rhapsody",
+    artist: "Queen",
+    year: 1975,
+    genre: "Rock",
+  },
+  {
+    id: 2,
+    title: "Sweet Child O' Mine",
+    artist: "Guns N' Roses",
+    year: 1987,
+    genre: "Rock",
+  },
+];
 
-    
-   const movie = movies.find(el => el.id === id) // find parameter id and match it with file id
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      movies: movie,
-    },
-  });
-});
-
-app.post('/api/v1/movies', (req, res) =>{
-    // console.log(req.body);
-
-    const newId = movies[movies.length -1].id+1;
-    const newMovie =Object.assign({id: newId}, req.body);
-
+app
+  .route("/movies")
+  .get((req, res) => res.json(movies))
+  .post((req, res) => {
+    const newId = movies[movies.length - 1].id + 1;
+    const newMovie = Object.assign({ id: newId }, req.body);
     movies.push(newMovie);
-    fs.writeFile(`${__dirname}/the-data/movies.json`, JSON.stringify(movies), err => {
-      res.status(201).json({
-        status:'success',
-        data: {
-            movies:newMovie
-        }
-      });
+    res.json(movies);
+  })
+    .delete((req, res) => {
+        const {id} = req.body;
+        const index = movies.findIndex(m => m.id === id);
+        if (index === -1) return res.status(404).json({error: 'movie not found'});
+        movies.splice(index, 1);
+        res.json(movies);
+    })
+    .put((req, res) => {
+      const {id, ...updateData} = req.body;
+      const index = movies.findIndex(m => m.id === id);
+      if (index === -1) return res.status(404).json({err: 'movie not found'});
+      movies[index] = {...movies[index], ...updateData };
+      res.json(movies);
+
     });
 
-});
+    app.route('/series')
+    .get((req, res) => res.json(series))
+    .post((req, res) => {
+      const newId = series[series.length-1].id+1;
+      const newSeries = Object.assign({id: newId}, req.body);
+      series.push(newSeries);
+      res.json(series);
 
-
-///////////////////////////////////////////////////////////////
-//songs
-//////////////////////////////////////////////////////////////
-const songs =JSON.parse(fs.readFileSync(`${__dirname}/the-data/songs.json`));
-
-app.get('/api/v1/songs', (req, res) =>{
-    res.status(200).json({
-        status: 'success',
-        results:songs.length,
-        data:{
-            songs
-        }
+    })
+    .delete((req, res) => {
+      const {id} = req.body;
+      const index = series.findIndex(s => s.id === id);
+      if(index === -1){
+        return res.status(404).json({error:'series not found'});
+      };
+      series.splice(index, 1);
+      res.json(series);
+    })
+    .put((req, res) => {
+      const{id, ...updateData} = req.body;
+      const index = series.findIndex(s => s.id === id);
+      if(index === -1){
+        return res.status(404).json({error:'series not found'});
+      }
+      series[index] = {...series[index], ...updateData};
+      res.json(series);
     });
-});
 
-
-app.get("/api/v1/songs/:id", (req, res) => {
-const id = req.params.id*1;
-
-
-
-if(id>songs.length){
-    return res.status(404).json({
-        status:'failed',
-        message:'Invalid ID'
-    });
-};
-
- 
-   const song = songs.find(el => el.id === id);
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      songs: song
-    },
-});
-});
-
-
-
-
-app.post('/api/v1/songs', (req, res) =>{
-    const newId = songs[songs.length-1].id+1;  //create a new id
-    const newSong =Object.assign({id: newId},req.body); //merge the new id with req.body and save it to newsong
-    
-    movies.push(newSong); // push newmovie to movies
-    fs.writeFile(`${__dirname}/the-data/songs.json`, JSON.stringify(songs), err => {
-    res.status(201).json({
-        status: 'success',
-        data:{
-             songs:newSong
-        }
-    });
-    });
-});
-
-
-
-//////////////////////////////////////////////////////////////
-//serieses
-//////////////////////////////////////////////////////////////
-const serieses = JSON.parse(fs.readFileSync(`${__dirname}/the-data/serieses.json`));
-
-app.get('/api/v1/serieses', (req, res) => {
-    res.status(200).json({
-        status:'sucess',
-        results:serieses.length,
-        data:{
-            serieses:serieses
-
-        }
-    });
-});
-
-app.get("/api/v1/serieses/:id", (req, res) => {
-  const id = req.params.id * 1;
-
-  if (id > serieses.length) {
-    return res.status(404).json({
-      status: "failed",
-      message: "Invalid ID",
-    });
-  }
-
-  const series = serieses.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      serieses: series,
-    },
-  });
-});
-
-
-
-
-
+    app.route('/songs')
+    .get((req, res) => res.json(songs))
+    .post((req, res) => {
+       const newId = songs[songs.length-1].id+1;
+       const newSong = Object.assign({id: newId}, req.body);
+       songs.push(newSong);
+       res.json(songs);
+    })
+    .delete((req, res) => {
+      const {id} = req.body;
+      const index = songs.findIndex( s => s.id === id);
+      if(index === -1) {
+        res.status(404).json({error: 'song not found'})
+      };
+        songs.splice(index,1)
+        res.json(songs)
   
+    })
+    .put((req, res) =>{
+      const {id, ...updateData} = req.body;
+      const index = songs.findIndex(s => s.id === id);
+      if(id === -1) res.status(404).json({error:'song not found'});
+      songs[index] = {...songs[index], ...updateData };
+      res.json(songs)
+    })
 
+const Port = 3000;
+app.listen(Port, () => console.log(`server running on port ${Port}`))
 
-
-
-
-
-
-app.post('/api/v1/serieses', (req, res) => {
-const newId = serieses[serieses.length - 1].id + 1;
-const newSeries = Object.assign({ id: newId }, req.body);
-
-serieses.push(newSeries)
-fs.writeFile(`${__dirname}/the-data/serieses.json`, JSON.stringify(serieses), err => {
-    res.status(201).json({
-        status:'success',
-        data:{
-            serieses: newSeries
-        }
-    });
-});
-
-
-});
-
-
-
-
-const port = 8000;
-app.listen(port, () => {
-  console.log(`app running on port ${port}......`);
-});
